2ec9a1c9dd3f445752fa8dbb077262061456585e
quooctrangg <nguyenquoctrang188@gmail.com>
2022-10-08 20:47:49 +0700

    LAB3_bai1_bai2

--- Lab3/JS/index.js
+++ Lab3/JS/index.js
@@ -1,1 +1,19 @@
-alert("Hello World!");
+const runScript = (e) => {
+    if (e.keyCode === 13) {
+        search()
+        return false
+    }
+}
+
+const search = () => {
+    let valueText = document.getElementById('value__text')
+    let value = valueText.value
+    if (value.length) {
+        console.log(value)
+        window.location = 'timkiem.html'
+    }
+}
+
+const checkValue = (frm) => {
+    return frm.checkValidity();
+}
